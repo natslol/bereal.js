5f488605755972d31a9b051627a98b8febea44ed
Kumalala <askipmaildeouf@protonmail.com>
2023-04-11 00:01:02 +0200

    Update index.js

--- test/index.js
+++ test/index.js
@@ -1,64 +1,40 @@
 require('dotenv').config();
 const BeReal = new (require("../src/bereal.js"))(process.env.REFRESH_TOKEN)
 
-BeReal.get_friends_feeds(15).then(res => {
-    console.log(res)
-})
-
-BeReal.get_memories_feeds(1).then(res => {
-    console.log(res)
-})
-
-BeReal.get_fof_feeds(10).then(res => {
-    console.log(res)
-})
-
-BeReal.get_discovery_feeds(1).then(res => {
-    console.log(res)
-})
-
-BeReal.get_sent_friend_requests().then(res => {
-    console.log(res);
-})
-
-BeReal.get_received_friend_requests().then(res => {
-    console.log(res);
-})
-
-BeReal.search_username("username").then(res => {
-    console.log(res);
-})
-
-BeReal.get_reactions("postid").then((res) => {
-    console.log(res);
-})
-
-BeReal.get_friends_suggestions().then(res => {
-    console.log(res);
-})
-
-BeReal.get_friends_pics().then(res => {
-    console.log(res);
-})
-
-BeReal.get_friends().then(res => {
-    console.log(res);
-})
-
-BeReal.get_friends_with_location().then(res => {
-    console.log(res);
-})
-
-BeReal.get_user_profile("userid").then(res => {
-    console.log(res);
-})
-
 BeReal.get_user_info().then(res => {
     console.log(res);
 })
 
-
-
-
-
-
+/* EXAMPLE OUTPUT
+{
+  id: 'userid',
+  username: 'username',
+  birthdate: '2000-01-01T00:00:00.000Z',
+  fullname: 'Fullname',
+  profilePicture: {
+    url: 'url',
+    width: 500,
+    height: 500
+  },
+  realmojis: [],
+  devices: [
+    {
+      clientVersion: '0.65.0',
+      device: 'Redmi 21061119DG Android 12',
+      deviceId: '3bd19b9cd9aa6a96',
+      platform: 'android',
+      language: 'fr',
+      timezone: 'Europe/Paris'
+    }
+  ],
+  canDeletePost: true,
+  canPost: true,
+  canUpdateRegion: true,
+  phoneNumber: '+33612345678',
+  biography: "Who is god,
+  location: 'Here',
+  countryCode: 'FR',
+  region: 'europe-west',
+  createdAt: '2022-09-13T21:11:53.449Z'
+}
+*/
